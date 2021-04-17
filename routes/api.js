1d7a19c30aabd7a5038919718d0b12051dac43cb
Terrence Chan <terrencechan.writer@gmail.com>
2021-04-17 15:56:57 -0400

    Add delete route

--- routes/api.js
+++ routes/api.js
@@ -31,4 +31,15 @@ router.get("/api/transaction", (req, res) => {
     });
 });
 
+
+router.delete("/api/transaction/:_id", ({params}, res) => {
+  Transaction.findOneAndDelete(params._id)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
 module.exports = router;
