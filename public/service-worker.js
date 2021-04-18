6a42649b82e4fdf1e3ca485cd6a53edbe52d4f79
Terrence Chan <terrencechan.writer@gmail.com>
2021-04-18 05:57:35 -0400

    Add service worker for offline usage

---
+++ public/service-worker.js
@@ -0,0 +1,63 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    './index.html',
+    './css/styles.css',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
+    './js/idb.js',
+    './js/index.js'
+]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Installing cache : ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
+        }
+        )
+    )
+})
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            let cacheKeeplist = keyList.filter(key => {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map((key, i) => {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('Deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch request : ' + event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(request => {
+            if (request) {
+                console.log('Responding with cache : ' + event.request.url);
+                return request;
+            } else {
+                console.log('File is not cached, fetching : ' + event.request.url);
+                return fetch(event.request);
+            }
+        })
+    )
+})
