2112b01b816894f3d01e5ee3e9ac553fcf949499
Terrence Chan <terrencechan.writer@gmail.com>
2021-04-18 03:54:28 -0400

    Add offline syncing

---
+++ public/js/idb.js
@@ -0,0 +1,63 @@
+let db;
+const request = indexedDB.open('tracker', 1);
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+}
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadData();
+    }
+}
+
+request.onerror = (event) => {
+    console.log(event.target.errorCode);
+}
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    transactionObjectStore.add(record);
+}
+
+const uploadData = () => {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const transactionObjectStore = transaction.objectStore('new_transaction');
+                    transactionObjectStore.clear();
+
+                    console.log('Transactions have been uploaded live to the server. All items synced.');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+window.addEventListener('online', () => {
+    console.log('Connection re-established. Syncing items.')
+    uploadData();
+});
--- public/js/index.js
+++ public/js/index.js
@@ -136,7 +136,9 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
+    console.log(transaction);
     saveRecord(transaction);
+
 
     // clear form
     nameEl.value = "";
